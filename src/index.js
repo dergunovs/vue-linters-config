6a136c36f96348e48b69b47e64a142f21c41138b
Aleksandr Dergunov <dergunovs@mail.ru>
2024-09-25 17:13:30 +0300

    fix: import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { ignores, settings, rules } from './eslint';
+import { parser, options, ignores, settings, rules } from './eslint';
 import { prettier } from './prettier';
 import { stylelint } from './stylelint';
 
-export { ignores, settings, rules, prettier, stylelint };
+export { parser, options, ignores, settings, rules, prettier, stylelint };
